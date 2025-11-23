b6465663fd9aabd61f434b9d90645a7558d595a4
antonio-admin <antoniojgo1499@gmail.com>
2025-11-23 20:47:54 +0100

    START: Project Start, workspace created

---
+++ src/main/java/io/agomosodev/productmanagementapi/ProductmanagementapiApplication.java
@@ -0,0 +1,13 @@
+package io.agomosodev.productmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/agomosodev/productmanagementapi/ProductmanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agomosodev.productmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
